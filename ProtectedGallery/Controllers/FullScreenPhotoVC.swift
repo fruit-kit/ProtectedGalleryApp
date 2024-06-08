71e7c7cd027e05c4d93fe62f3d29b38d91ff6399
fruit-kit <tr3asur3island@gmail.com>
2024-06-08 10:13:07 +0300

    Add FullScreenPhotoVC and associated XIB file

---
+++ ProtectedGallery/Controllers/FullScreenPhotoVC.swift
@@ -0,0 +1,17 @@
+//
+//  FullScreenPhotoVC.swift
+//  ProtectedGallery
+//
+//  Created by Admin on 08.06.2024.
+//
+
+import UIKit
+
+class FullScreenPhotoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
